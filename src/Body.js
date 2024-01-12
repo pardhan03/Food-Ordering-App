f7657cbe915c8334151c20202f360ef29688a3e0
Mohitb07 <bmohit980@gmail.com>
2024-01-12 20:22:15 +0530

    refactor: Body component and fix restaurant issue

--- src/Body.js
+++ src/Body.js
@@ -1,80 +1,99 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./Restaurantcard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "./utils/useOnlineStatus";
 
-const Body = () =>{
+export const SWIGGY_API =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-    const [listofrestaurants,setListofRestaurant]=useState([]);
-    const [filteredRestaurant, setfilteredRestaurant]=useState([]);
+const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState([]);
+  const [filteredRestaurant, setfilteredRestaurant] = useState([]);
+  const [searchText, setsearchText] = useState();
+  const onlineStatus = useOnlineStatus();
 
-    const [searchText, setsearchText]=useState();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(SWIGGY_API);
+        const data = await res.json();
+        console.log(data);
+        const restaurants =
+          data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+            ?.restaurants;
+        if (restaurants) {
+          setListOfRestaurant(restaurants);
+          setfilteredRestaurant(restaurants);
+        }
+      } catch (error) {
+        console.log(error);
+        // do something with error
+      }
+    };
+    fetchData();
+  }, []);
 
-    useEffect(()=>{
-        fetchdata()
-    },[]);
+  const handleSearchInput = (e) => {
+    setsearchText(e.target.value);
+  };
 
-    const fetchdata = async () =>{
-        const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-        const json= await data.json();
-        console.log(json);
-        setListofRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        setfilteredRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        console.log(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-
-    }
-
-    const onlineStatus=useOnlineStatus();
-
-    if(onlineStatus===false) 
-    return( 
-        <h1>
-            Looks like you're offline
-        </h1>
+  const handleSearch = () => {
+    const filteredRestaurant = listOfRestaurant.filter((res) =>
+      res.info.name.toLowerCase().includes(searchText.toLowerCase())
     );
+    setfilteredRestaurant(filteredRestaurant);
+  };
 
-    return listofrestaurants.length === 0 ? (
-        <Shimmer />
-      ) : (
-        <div className="Body">
-            <div className="filter flex">
-                <div className=" m-4 p-4">
-                    <input type="text" className="border border-solid border-black" value={searchText} 
-                    onChange={(e)=>{
-                        setsearchText(e.target.value);
-                    }}/>
-                        <button className="px-4 py-2 bg-green-100 m-4 rounded-lg"
-                        onClick={()=>{
-                            const filteredRestaurant=listofrestaurants.filter((res)=>
-                                res.info.name.toLowerCase().includes(searchText.toLowerCase()));
-                            setfilteredRestaurant(filteredRestaurant);
-                        }}>Search</button>
-                </div>
-                <div className="search m-4 p-4 flex items-center">
-                <button
-                    className="px-4 py-2 bg-green-100 m-4 rounded-lg"
-                    onClick={()=>{
-                        const filteredList=listofrestaurants.filter((res)=>res.data.avgRating>4);
-                        setListofRestaurant(filteredList);
-                    }}
-                >
-                    Top Rated estaurant
-                </button>
-                </div>
-            </div>
-            <div className="flex flex-wrap">
+  const handleTopRatedSearch = () => {
+    const filteredList = listOfRestaurant.filter(
+      (res) => res.data.avgRating > 4
+    );
+    setListOfRestaurant(filteredList);
+  };
+
+  if (!onlineStatus) return <h1>Looks like you're offline</h1>;
+
+  if (!listOfRestaurant.length) return <Shimmer />;
+
+  return (
+    <div className="Body">
+      <div className="filter flex">
+        <div className=" m-4 p-4">
+          <input
+            type="text"
+            className="border border-solid border-black"
+            value={searchText}
+            onChange={handleSearchInput}
+          />
+          <button
+            className="px-4 py-2 bg-green-100 m-4 rounded-lg"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
+        <div className="search m-4 p-4 flex items-center">
+          <button
+            className="px-4 py-2 bg-green-100 m-4 rounded-lg"
+            onClick={handleTopRatedSearch}
+          >
+            Top Rated Restaurant
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-wrap">
         {filteredRestaurant.map((restaurant) => (
           <Link
             key={restaurant.info.id}
-            to={"/restaurant/" + restaurant.info.id}
+            to={`/restaurant/${restaurant.info.id}`}
           >
             <RestaurantCard resData={restaurant} />
           </Link>
         ))}
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Body;
